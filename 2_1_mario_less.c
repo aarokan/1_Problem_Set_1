ded7666ca3a79ccc8f48855b65bd0db6feff2d29
AR <aarokan@gmail.com>
2023-10-30 20:17:26 +0300

    build the opposite

--- 2_1_mario_less.c
+++ 2_1_mario_less.c
@@ -6,7 +6,21 @@
 int main(void)
 {
     // Prompt height input 1-8
-    // Loop from 0 to height
-        // Loop from 0 to current counter
-        // print #
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 9);
+    
+    // Loop from 0 to height (col)
+    for (int row = 0; row < height; row++)
+    {
+        // Loop from 0 to current counter (row)
+        for (int col = 0; col <= row; col++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
