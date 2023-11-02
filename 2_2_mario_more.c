98edb6c161519b1ee4a79bcd0574cd36c46d90fc
AR <aarokan@gmail.com>
2023-11-02 23:44:07 +0300

    write psuedo code

--- 2_2_mario_more.c
+++ 2_2_mario_more.c
@@ -5,5 +5,15 @@
 
 int main(void)
 {
-    
+    /*  
+    Prompt user for height
+    If height is less than 1 or greater than 8 
+    (or not an integer at all), go back one step 
+    */
+
+   /*  
+    Iterate from 1 through height. On iteration i, print:
+    (required empty space + i hashes + double space + i hashes)
+    and then a newline 
+    */
 }
