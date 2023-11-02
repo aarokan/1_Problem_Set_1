9f3f1019970aba1e5c86eda3de705794869f2e08
AR <aarokan@gmail.com>
2023-11-02 23:59:40 +0300

    create col loop

--- 2_2_mario_more.c
+++ 2_2_mario_more.c
@@ -5,8 +5,25 @@
 
 int main(void)
 {
-    // Prompt height input 1-8        
+    // Prompt height input 1-8
+    int height;        
+    do
+    {
+        height = get_int("Height: ");
+    } 
+    while (height < 1 || height > 8);
+    
     // Loop from 0 to height (col)
+    for (int col = 0; col < height; col++)
+    {
         // Right-align pyramid by printing required empty space 
+        int requiredEmptySpace = height - col + 1;
+        while (requiredEmptySpace > 0)
+        {
+            /* code */
+        }
+        
+
         // print (i hashes + double space + i hashes)
+    }
 }
