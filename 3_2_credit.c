bb4bb9883e4adadadf529e9efb82f228d95256f7
AR <aarokan@gmail.com>
2023-11-15 22:14:33 +0300

    check modulo 10 to get last digit

--- 3_2_credit.c
+++ 3_2_credit.c
@@ -4,9 +4,18 @@
 int main(void) 
 {
     // prompt for input (long) 
-
+    long cardNumber;
+    do
+    {
+        cardNumber = get_long("Credit Card Number: ");
+    }
+    while (cardNumber < 999999999999 || cardNumber > 9999999999999999);
+    
     // check if credit card number is valid (calculate Checksum)
     /* note: if we take a big number % 10 it will give us last digit*/
+    printf("%li", cardNumber % 10);
+    printf("\n%li", (cardNumber % 100 - cardNumber %10) / 10);
+    printf("\n%li", (cardNumber % 1000 - cardNumber % 100) / 100);
         // Multiply every other digit by 2, starting with the number’s second-to-last
         // then add those products’ digits together (13 would be 1+3) sumOfMultiply
         // add digits that weren’t multiplied by 2 (starting from the end) sumOfRemainingDigits
@@ -15,5 +24,5 @@ int main(void)
     // check for card length & starting digit (VISA or MASTERCARD ..etc
     
     // print AMEX, MASTERCARD, VISA or INVALID
-    
+
 }
